212be67f09a91a8a1b0f1765fd6e5d2e1a404ec4
ShinuShaju <shinushaju.ac@gmail.com>
2022-07-05 17:09:33 +0000

    Added scripts to handle failures, updated comments, log messages, renamed log file.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,16 +13,18 @@ cfn_init_status() {
     exit 1
 }
 
-# Checking AWS cli version
-aws --version
-
 # Enable EPEL repo
 qs_enable_epel &> /var/log/userdata.qs_enable_epel.log
 
 cd /tmp
 
-# Installing AWS SSH agent
+# Installing AWS SSM agent
 qs_retry_command 10 wget https://s3-us-west-1.amazonaws.com/amazon-ssm-us-west-1/latest/linux_amd64/amazon-ssm-agent.rpm
+rc=$?
+if [ "$rc" != "0" ]; then
+  failure_msg="[ERROR] Couldn't download Amazon SSM agent."
+  cfn_init_status "$failure_msg"
+fi
 qs_retry_command 10 yum install -y ./amazon-ssm-agent.rpm
 systemctl start amazon-ssm-agent
 systemctl enable amazon-ssm-agent
@@ -32,21 +34,31 @@ if [ ! -d "/usr/local/bin/" ]; then
   mkdir /usr/local/bin/
   rc=$?
   if [ "$rc" != "0" ]; then
-    failure_msg="[Error] Couldn't create /usr/local/bin/ directory."
+    failure_msg="[ERROR] Couldn't create /usr/local/bin/ directory."
     cfn_init_status "$failure_msg"
   fi
 fi
 
-# Installing Red Hat Openshift CLI
+# Installing Red Hat Openshift 4.8 CLI
 qs_retry_command 10 wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.8/openshift-client-linux.tar.gz
+rc=$?
+if [ "$rc" != "0" ]; then
+  failure_msg="[ERROR] Couldn't download Red Hat Openshift CLI."
+  cfn_init_status "$failure_msg"
+fi
 tar -xvf openshift-client-linux.tar.gz
 chmod 755 oc
 mv oc /usr/local/bin/oc
 rm -rf kubectl
 rm -f openshift-client-linux.tar.gz
 
-# Installing Red Hat Openshift installer
+# Installing Red Hat Openshift 4.8 installer
 qs_retry_command 10 wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.8/openshift-install-linux.tar.gz
+rc=$?
+if [ "$rc" != "0" ]; then
+  failure_msg="[ERROR] Couldn't download Red Hat Openshift installer."
+  cfn_init_status "$failure_msg"
+fi
 tar -xvf openshift-install-linux.tar.gz
 chmod 755 openshift-install
 mv openshift-install /ibm
@@ -54,19 +66,29 @@ rm -f openshift-install-linux.tar.gz
 
 cd ..
 
-# Installing docker engine
+# Installing Docker CLI 19.x.x
 wget https://download.docker.com/linux/static/stable/x86_64/docker-19.03.9.tgz
+rc=$?
+if [ "$rc" != "0" ]; then
+  failure_msg="[ERROR] Couldn't download Docker CLI."
+  cfn_init_status "$failure_msg"
+fi
 tar -xvf docker-19.03.9.tgz 
 cp docker/* /usr/local/bin/
 dockerd &> /dev/null &
 rm -f ./docker-19.03.9.tgz
 
-# Testing docker
-ps -ef |grep docker
+# Testing Docker
+ps -ef | grep docker
 docker run hello-world
 
-# Installing cloudctl
+# Installing Cloud Pak CLI latest version
 curl -L https://github.com/IBM/cloud-pak-cli/releases/latest/download/cloudctl-linux-amd64.tar.gz -o cloudctl-linux-amd64.tar.gz
+rc=$?
+if [ "$rc" != "0" ]; then
+  failure_msg="[ERROR] Couldn't download Cloud Pak CLI."
+  cfn_init_status "$failure_msg"
+fi
 tar -xvf cloudctl-linux-amd64.tar.gz
 chmod 755 cloudctl-linux-amd64
 mv cloudctl-linux-amd64 /usr/local/bin/cloudctl
@@ -74,23 +96,33 @@ rm -f cloudctl-linux-amd64.tar.gz
 
 # Installing jq
 wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O /usr/local/bin/jq
+rc=$?
+if [ "$rc" != "0" ]; then
+  failure_msg="[ERROR] Couldn't download jq."
+  cfn_init_status "$failure_msg"
+fi
 chmod 755 /usr/local/bin/jq
 
 # Installing yq
 wget https://github.com/mikefarah/yq/releases/download/3.4.0/yq_linux_386 -O /usr/local/bin/yq
+rc=$?
+if [ "$rc" != "0" ]; then
+  failure_msg="[ERROR] Couldn't download yq."
+  cfn_init_status "$failure_msg"
+fi
 chmod 755  /usr/local/bin/yq
 
 # Installed boto3
 qs_retry_command 10 pip install boto3 &> /var/log/userdata.boto3_install.log
 
-# Downloading Quick Start assets from s3 bucket
+# Downloading Quick Start assets from S3 bucket
 
 if [ -n "$CP4S_QS_S3URI" ]; then
   # Downloading Quick Start scripts from S3 bucket
   aws s3 cp ${CP4S_QS_S3URI}scripts/ /ibm/ --recursive
   rc=$?
   if [ "$rc" != "0" ]; then
-    failure_msg="[Error] Quick Start scripts couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
+    failure_msg="[ERROR] Quick Start scripts couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
     cfn_init_status "$failure_msg"
   fi
 fi
@@ -100,7 +132,7 @@ if [ -n "$DOMAIN_CERTIFICATE_S3URI" ]; then
   aws s3 cp ${DOMAIN_CERTIFICATE_S3URI} /ibm/tls/tls.crt
   rc=$?
   if [ "$rc" != "0" ]; then
-    failure_msg="[Error] TLS certificate couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
+    failure_msg="[ERROR] TLS certificate couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
     cfn_init_status "$failure_msg"
   fi
 fi
@@ -110,7 +142,7 @@ if [ -n "$DOMAIN_CERTIFICATE_KEY_S3URI" ]; then
   aws s3 cp ${DOMAIN_CERTIFICATE_KEY_S3URI} /ibm/tls/tls.key
   rc=$?
   if [ "$rc" != "0" ]; then
-    failure_msg="[Error] TLS certificate key couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
+    failure_msg="[ERROR] TLS certificate key couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
     cfn_init_status "$failure_msg"
   fi
 fi
@@ -120,7 +152,7 @@ if [ -n "$CUSTOM_CA_FILE_S3URI" ]; then
   aws s3 cp ${CUSTOM_CA_FILE_S3URI} /ibm/tls/ca.crt
   rc=$?
   if [ "$rc" != "0" ]; then
-    failure_msg="[Error] Custom TLS certificate couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
+    failure_msg="[ERROR] Custom TLS certificate couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
     cfn_init_status "$failure_msg"
   fi
 fi
@@ -130,7 +162,7 @@ if [ -n "$SOAR_ENTITLEMENT" ]; then
   aws s3 cp ${SOAR_ENTITLEMENT} /ibm/license.key
   rc=$?
   if [ "$rc" != "0" ]; then
-    failure_msg="[Error] SOAR Entitlement couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
+    failure_msg="[ERROR] SOAR entitlement couldn't be downloaded from S3 bucket. Invalid S3 endpoint URI."
     cfn_init_status "$failure_msg"
   fi
 fi
@@ -140,7 +172,7 @@ if [ ! -d "${PWD}/logs" ]; then
   mkdir logs
   rc=$?
   if [ "$rc" != "0" ]; then
-    failure_msg="[Error] Couldn't create ${PWD}/logs directory."
+    failure_msg="[ERROR] Couldn't create ${PWD}/logs directory."
     cfn_init_status "$failure_msg"
   fi
 fi
@@ -159,7 +191,7 @@ if [ -f "/ibm/license.key" ]; then
   chmod 755 license.key
 fi
 
-LOGFILE="${PWD}/logs/bootstrap.log--`date +'%Y-%m-%d_%H-%M-%S'`"
+LOGFILE="${PWD}/logs/bootstrap.log"
 echo $HOME
 export KUBECONFIG=/root/.kube/config
 echo $KUBECONFIG
